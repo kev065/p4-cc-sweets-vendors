ac7857edc04c3fb2ed99dc87ea2d3bf223146d2b
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-19 18:48:32 +0300

    Add new models and schemas
    Add 'name' field to Vendor model.
    
    Create new Sweet and VendorSweet models.
    
    Create SQLAlchemyAutoSchema for Vendor, Sweet, and VendorSweet.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,41 @@
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 db = SQLAlchemy()
 
 class Vendor(db.Model):
     __tablename__ = 'vendor'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    vendor_sweets = db.relationship('VendorSweet', backref='vendor', lazy=True)
 
-# add any models you may need. 
+class Sweet(db.Model):
+    __tablename__ = 'sweet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    vendor_sweets = db.relationship('VendorSweet', backref='sweet', lazy=True)
+
+class VendorSweet(db.Model):
+    __tablename__ = 'vendor_sweet'
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), nullable=False)
+    sweet_id = db.Column(db.Integer, db.ForeignKey('sweet.id'), nullable=False)
+
+class VendorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Vendor
+        include_relationships = True
+        load_instance = True
+
+class SweetSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Sweet
+        include_relationships = True
+        load_instance = True
+
+class VendorSweetSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = VendorSweet
+        include_relationships = True
+        load_instance = True
