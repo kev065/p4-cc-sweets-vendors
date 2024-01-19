5b02448bd3a9333cf7f22b4e04aa4b967ff3fe51
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-19 22:06:28 +0300

    Add timestamp fields to Vendor, Sweet, and VendorSweet models
    Add 'created_at' and 'updated_at' fields to the Vendor, Sweet, and VendorSweet models.
    
    Set the default value of 'created_at' and 'updated_at' to the EAT timezone
    
    Update 'updated_at' field automatically whenever the record is updated. - This is achieved by using the 'onupdate' attribute of the SQLAlchemy 'Column' function.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from sqlalchemy.orm import validates
+from datetime import datetime
+from pytz import timezone
 
 db = SQLAlchemy()
 
@@ -9,12 +11,16 @@ class Vendor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     vendor_sweets = db.relationship('VendorSweet', backref='vendor', lazy=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone('Africa/Nairobi')))
+    updated_at = db.Column(db.DateTime, default=datetime.now(timezone('Africa/Nairobi')), onupdate=datetime.now(timezone('Africa/Nairobi')))
 
 class Sweet(db.Model):
     __tablename__ = 'sweet'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     vendor_sweets = db.relationship('VendorSweet', backref='sweet', lazy=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone('Africa/Nairobi')))
+    updated_at = db.Column(db.DateTime, default=datetime.now(timezone('Africa/Nairobi')), onupdate=datetime.now(timezone('Africa/Nairobi')))
 
 class VendorSweet(db.Model):
     __tablename__ = 'vendor_sweet'
@@ -22,6 +28,8 @@ class VendorSweet(db.Model):
     price = db.Column(db.Float, nullable=False)
     vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), nullable=False)
     sweet_id = db.Column(db.Integer, db.ForeignKey('sweet.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone('Africa/Nairobi')))
+    updated_at = db.Column(db.DateTime, default=datetime.now(timezone('Africa/Nairobi')), onupdate=datetime.now(timezone('Africa/Nairobi')))
 
     @validates('price')
     def validate_price(self, key, price):
@@ -30,7 +38,6 @@ class VendorSweet(db.Model):
         if price < 0:
             raise AssertionError('Price cannot be a negative number')
         return price
-
 
 class VendorSchema(SQLAlchemyAutoSchema):
     class Meta:
